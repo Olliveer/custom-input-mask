f3ba6da99e4e633d301b009e1f92e3aef9991da0
João Vitor <j.olliveira98@outlook.com>
2023-09-16 19:09:02 -0300

    fix: input reference for input mask component

--- src/components/input-mask.tsx
+++ src/components/input-mask.tsx
@@ -27,14 +27,7 @@ const CustonInput = React.forwardRef<HTMLInputElement, InputProps>(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        // TODO: fix this ref issue
-        // (property) React.ClassAttributes<ReactInputMask>.ref?: React.LegacyRef<InputMask> | undefined
-        // Allows getting a ref to the component instance. Once the component unmounts, React will set ref.current to null (or call the ref with null if you passed a callback ref).
-        // @see — https://react.dev/learn/referencing-values-with-refs#refs-and-the-dom
-
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        ref={ref}
+        inputRef={ref}
         {...props}
       />
     );
